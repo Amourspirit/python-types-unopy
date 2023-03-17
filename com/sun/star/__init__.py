9b16442c8d2613e4fd8f417d13b9f8a359fafe06
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-03-17 18:13:47 -0400

    upgrade to poetry

--- com/sun/star/__init__.py
+++ com/sun/star/__init__.py
@@ -8,5 +8,5 @@ __version_tmpl__ = "0.2.11"
 _IGNORE_IMPORT_ERROR = os.environ.get(
     "ooouno_ignore_import_error", None) in ("True", "true", "yes")
 
-if TYPE_CHECKING == False and _IGNORE_IMPORT_ERROR == False:
+if TYPE_CHECKING is False and _IGNORE_IMPORT_ERROR is False:
     raise ImportError
