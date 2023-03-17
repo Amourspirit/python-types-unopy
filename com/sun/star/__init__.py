a233dd87988f0416c751cf44c74271276c2b7103
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-03-17 18:17:25 -0400

    LO 7.4 pyi files

--- com/sun/star/__init__.py
+++ com/sun/star/__init__.py
@@ -2,8 +2,8 @@
 import os
 from typing import TYPE_CHECKING
 
-__version__ = "0.4.7"
-__version_tmpl__ = "0.2.11"
+__version__ = "1.0.0"
+__version_tmpl__ = "0.2.15"
 
 _IGNORE_IMPORT_ERROR = os.environ.get(
     "ooouno_ignore_import_error", None) in ("True", "true", "yes")
